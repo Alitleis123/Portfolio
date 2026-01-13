198e5cffb6a035d94c05a4e54531b4a300b82ae1
Alitleis123 <alitleis0731@gmail.com>
2026-01-12 22:24:46 -0500

    a

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
